84301473caef975d84cba2b97844e621d299b7f0
Kamil Ziętek <qamil1995@gmail.com>
2020-03-28 10:40:27 +0100

    Add project files.

---
+++ KolejarzCs/Program.cs
@@ -0,0 +1,30 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+
+namespace KolejarzCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderWindow window = new RenderWindow(new VideoMode(200, 200), "SFML works!");
+            CircleShape shape = new CircleShape(100);
+            shape.FillColor = Color.Green;
+
+            window.Closed += Window_Closed;
+
+            while (window.IsOpen)
+            {
+                window.Clear();
+                window.Draw(shape);
+                window.Display();
+            }
+        }
+
+        private static void Window_Closed(object sender, EventArgs e)
+        {
+            (sender as Window).Close();
+        }
+    }
+}
---
+++ KolejarzCs/Station/Station.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace KolejarzCs.Station
+{
+    class Station
+    {
+        int Height { get; }
+
+        int Width { get; }
+
+        List<StationElement> StationElements { get; }
+
+    }
+}
